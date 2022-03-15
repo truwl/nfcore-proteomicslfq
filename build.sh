836dd292ccd1d1b037ebb163685c2c708663a6cc
Jeremy Leipzig <leipzig@gmail.com>
2022-03-14 22:29:59 -0600

    bdcli build

---
+++ build.sh
@@ -0,0 +1,4 @@
+docker build -t truwl/nfcore-proteomicslfq:1.0.0_0.1.0 -t truwl/nfcore-proteomicslfq:latest .
+docker push truwl/nfcore-proteomicslfq:1.0.0_0.1.0
+docker push truwl/nfcore-proteomicslfq:latest
+        
